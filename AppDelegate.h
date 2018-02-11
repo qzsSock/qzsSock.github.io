788bcc25a0a546f7ce9207c6a0df18afb5a7e60e
bobqiuzishuo <625008541@qq.com>
2018-02-11 11:57:37 +0800

    8

---
+++ AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  CAEmitter
+//
+//  Created by 邱子硕 on 2018/2/5.
+//  Copyright © 2018年 邱子硕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
